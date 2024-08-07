13627cccc314e8d54738b2caf36df81c0c5d9ace
ahyun <kaeruruk@naver.com>
2024-08-07 17:38:39 +0900

    greenblueredgreen/instargram_240729#10    프로필 편집 마크업, controller 연동

--- src/main/java/com/Instargram/profile/ProfileController.java
+++ src/main/java/com/Instargram/profile/ProfileController.java
@@ -13,4 +13,10 @@ public class ProfileController {
 	public String profileView() {
 		return "/profile/profile";
 	}
+	
+	//프로필 편집 화면
+	@GetMapping("/profile-edit-view")
+	public String profileEdit() {
+		return "/profile/profileEdit";
+	}
 }
