2c48f661441eea4d43b4381c271541b11bcb67e2
ahyun <kaeruruk@naver.com>
2024-08-29 18:45:57 +0900

    greenblueredgreen/instargram_240729#30 카카오톡 로그인 구현

---
+++ src/main/java/com/Instargram/kakao/KakaoLoginController.java
@@ -0,0 +1,34 @@
+package com.Instargram.kakao;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Instargram.kakao.bo.KakaoService;
+import com.Instargram.kakao.domain.KakaoUserInfoResponseDto;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("")
+public class KakaoLoginController {
+
+    private final KakaoService kakaoService;
+
+    @GetMapping("/callback")
+    public ResponseEntity<?> callback(@RequestParam("code") String code) {
+        String accessToken = kakaoService.getAccessTokenFromKakao(code);
+
+        KakaoUserInfoResponseDto userInfo = kakaoService.getUserInfo(accessToken);
+
+        // User 로그인, 또는 회원가입 로직 추가
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/Instargram/kakao/domain/KakaoLoginDto.java
@@ -0,0 +1,14 @@
+package com.Instargram.kakao.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Builder
+@Getter
+public class KakaoLoginDto {
+
+    public String accessToken;
+    public String refreshToken;
+}
